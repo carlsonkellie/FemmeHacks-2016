c4252bc5021fb4395d36f315269f6ca3711d6a6b
Jaimie <jaimiec@seas.upenn.edu>
2016-02-20 15:15:47 -0500

    working on the college list page

--- FemmeFratAlly/app/src/main/java/com/example/kelliecarlson/femmefratally/FindASchool.java
+++ FemmeFratAlly/app/src/main/java/com/example/kelliecarlson/femmefratally/FindASchool.java
@@ -1,10 +1,73 @@
 package com.example.kelliecarlson.femmefratally;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.Firebase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jaimie on 2/20/2016.
  */
 public class FindASchool extends AppCompatActivity {
+    Firebase myFirebaseRef = new Firebase("https://blistering-torch-4059.firebaseio.com/");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+        setContentView(R.layout.find_a_school);
+
+
+        List<Firebase> arrayList = myFirebaseRef.child("colleges");
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+
+            }
+        }
+    });
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void seeColleges() {
+        Firebase colleges = myFirebaseRef.child("colleges");
+    }
+
+    public void backButton(View view) {
+        Intent intent = new Intent (this, LoginScreen.class);
+        startActivity(intent);
+
+    }
 
 }
